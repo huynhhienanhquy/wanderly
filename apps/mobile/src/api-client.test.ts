25b25632b7d79afa0cb2e821b367be8782df79c7
huynhhienanhquy2005 <quyhuynh.tp2005@email.com>
2026-08-20 21:09:37 +0700

    test(mobile): remove invalid client module test

--- apps/mobile/src/api-client.test.ts
+++
@@ -1,7 +0,0 @@
-import { mobileApiClient } from './api-client';
-
-describe('mobile API client', () => {
-  it('is configured with the mobile runtime API and retry transport', () => {
-    expect(mobileApiClient).toBeDefined();
-  });
-});
